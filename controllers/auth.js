c1d6c94942f0511b6c98e22174fc78f3bf16b662
Prasanna Nimbalkar <prasannanimbalkar163@gmail.com>
2023-09-21 13:16:57 -0400

    feat: added user login and used bcrypt for hashing and retriving passwords

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js"
 import bcrypt from "bcrypt"
+import { createError } from "../utils/error.js";
 
 export const register = async (req, res, next) => {
     try {
@@ -15,6 +16,21 @@ export const register = async (req, res, next) => {
 
         await newUser.save()
         res.status(200).send("User has been created")
+    } catch (err) {
+        next(err)
+    }
+}
+
+export const login = async (req, res, next) => {
+    try {
+        const user = await User.findOne({username: req.body.username })
+        if (!user) return next(createError(404, "User not found"))
+        
+        const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
+        if (!isPasswordCorrect) return next(createError(400, "wrong password or username"))
+
+        const { password, isAdmin, ...args } = user._doc;
+        res.status(200).json({ ...args})
     } catch (err) {
         next(err)
     }
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { register } from '../controllers/auth.js';
+import { login, register } from '../controllers/auth.js';
 
 const router = express.Router();
 
 //register user
 router.post('/register', register)
+router.get('/login', login)
 
 export default router
