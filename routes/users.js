408781dde2f6c8b3616f3363540b09b6271254a2
Prasanna Nimbalkar <prasannanimbalkar163@gmail.com>
2023-09-21 16:18:10 -0400

    feat: updated the authorization to user and hotel routes

--- routes/users.js
+++ routes/users.js
@@ -5,28 +5,28 @@ import { verifyAdmin, verifyToken, verifyUser } from '../utils/verifyToken.js';
 const router = express.Router();
 
 // read the api documention in postman user auth request
-router.get("/checkAuthentication", verifyToken, (req, res, next) => {
-    res.send("Hello user, you are logged in");
-})
+// router.get("/checkAuthentication", verifyToken, (req, res, next) => {
+//     res.send("Hello user, you are logged in");
+// })
 
-router.get("/checkUser/:id", verifyUser , (req, res, next) => {
-    res.send("Hello user, you are logged in and you can delete your account");
-})
+// router.get("/checkUser/:id", verifyUser , (req, res, next) => {
+//     res.send("Hello user, you are logged in and you can delete your account");
+// })
 
-router.get("/checkAdmin/:id", verifyAdmin , (req, res, next) => {
-    res.send("Hello Admin, you are logged in and you can delete all account");
-})
+// router.get("/checkAdmin/:id", verifyAdmin , (req, res, next) => {
+//     res.send("Hello Admin, you are logged in and you can delete all account");
+// })
 
 //Update
-router.put('/:id', updateUser)
+router.put('/:id',verifyUser, updateUser)
 
 //Delete
-router.delete('/:id', deleteUser)
+router.delete('/:id',verifyUser, deleteUser)
 
 //Get
-router.get('/:id', getUser)
+router.get('/:id',verifyUser, getUser)
 
 //Get All
-router.get('/', getAllUsers)
+router.get('/',verifyAdmin,  getAllUsers)
 
 export default router
